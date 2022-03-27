ad6d0c22989d80715705d514460473cee0ad9273
kadyfarrell <98439217+kadyfarrell@users.noreply.github.com>
2022-03-26 22:53:39 -0400

    Added CS,JS,and HTML

--- js/app.js
+++ js/app.js
@@ -1,4 +1,26 @@
-window.onload = () => {
-    alert(`JavaScript Works`);
-};
+function whiteRice() {
+  var text = document.getElementById("recipeOne");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+function caliRice() {
+  var text = document.getElementById("recipeTwo");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+document
+.getElementById('rice')
+.addEventListener('keyup', function (e) {
+  let value = Number(e.target.value)
+  document.getElementById('result').innerHTML = value * 2
+  document.getElementById('softResult').innerHTML = value * 2.5
+})
+
 
